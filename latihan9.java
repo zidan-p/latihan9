8653e1edfa648aa8fc85df3cb4d91d8e9f7241f3
Zidan Putra Rahman <68506833+zidan-p@users.noreply.github.com>
2021-01-09 09:14:38 +0700

    Add files via upload

---
+++ latihan9.java
@@ -0,0 +1,14 @@
+
+package latihanPBO;
+
+
+public class latihan9 {
+
+
+    public static void main(String[] args) {
+        System.out.println("SEmangat Pagi!!");  //mnampilkan kalimat
+        for(int i = 1; i < 4; i++)  // menggunakan perulangan dengan mendeklarasikan varibel i, jika i kurang dari 4 lakukan perulangan, setiap perulangan tammbah 1 niali i 
+            System.out.println("Pagi!!");   //menampilkan kalimat setiapperulangan
+    }
+    
+}
